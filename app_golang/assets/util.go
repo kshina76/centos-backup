80565e083c620076a3e11d123fe44db8942cdfe3
kshina76 <koushina47@gmail.com>
2020-11-24 21:47:52 +0900

    add

---
+++ app_golang/assets/util.go
@@ -0,0 +1,16 @@
+package util
+
+import (
+	"net/http"
+	"html/template"
+)
+
+func generateHTML(writer http.ResponseWriter, data interface{}, filenames ...string) {
+	var files []string
+	for _, file := range filenames {
+		files = append(files, fmt.Sprintf("templates/%s.html", file))
+	}
+
+	templates := template.Must(template.ParseFiles(files...))
+	templates.ExecuteTemplate(writer, "layout", data)
+}
